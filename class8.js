b8dfae947807da337a75dc140ac5351e13be66ce
Himani Sharma <99091480+himani0004@users.noreply.github.com>
2023-07-20 12:12:51 +0530

    Add files via upload

---
+++ class8.js
@@ -0,0 +1,30 @@
+
+//Generator function-------------------->>>>>>>>>>>>>>>
+
+// function* generateNo() {
+//     yield 1;
+//     yield 2;
+//     console.log("test")
+//     yield 3;
+//     yield 8;
+//   }
+//   // Create a generator object
+//   const numberGenerator = generateNo();
+  
+// Using the generator to get values
+//   console.log(numberGenerator.next().value); 
+//   console.log(numberGenerator.next().value); 
+//   console.log(numberGenerator.next().value); 
+//   console.log(numberGenerator.next().value);
+
+
+
+//First class function in javascript---------------------->>>>>>>>>>>>>>
+// function numPrint(a,b){
+//     return(a+b)
+// }
+// console.log(numPrint(5,6))
+// const add = numPrint
+// const  sub = add
+// console.log(typeof(sub))
+// console.log(sub(5,6))
